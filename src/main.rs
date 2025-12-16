0a31f4cddd6f9aac90526ca5d467b2a3395a78c5
Jeremie Corbier <jeremie.corbier@gmail.com>
2025-12-16 09:18:31 +0100

    fix: load application icon at runtime too
    Signed-off-by: Jeremie Corbier <jeremie.corbier@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,14 @@ mod models;
 use eframe::egui;
 
 fn main() -> eframe::Result<()> {
+    // Load application icon
+    let icon_data = load_icon();
+
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([1000.0, 700.0])
-            .with_min_inner_size([800.0, 600.0]),
+            .with_min_inner_size([800.0, 600.0])
+            .with_icon(icon_data),
         ..Default::default()
     };
 
@@ -17,4 +21,17 @@ fn main() -> eframe::Result<()> {
         options,
         Box::new(|cc| Ok(Box::new(app::VacDownloaderApp::new(cc)))),
     )
+}
+
+fn load_icon() -> egui::IconData {
+    let icon_bytes = include_bytes!("../assets/icons/256x256.png");
+    let image = image::load_from_memory(icon_bytes)
+        .expect("Failed to load application icon")
+        .to_rgba8();
+
+    egui::IconData {
+        rgba: image.as_raw().clone(),
+        width: image.width(),
+        height: image.height(),
+    }
 }
